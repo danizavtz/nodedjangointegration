8d2b40ea1a90eeeee43de113ae84312055dddc1e
danizavtz <daniellucena@yahoo.com.br>
2024-07-02 22:29:44 -0300

    remove linebreaks from text

--- server/controllers/servicestatus.controller.js
+++ server/controllers/servicestatus.controller.js
@@ -13,10 +13,12 @@ exports.status = (req, res) => {
         const outputarr = stdout.split(',');
         if (outputarr.length > 0 && outputarr[0].includes('RUNNING') && outputarr[0].includes(process.env.APP_NAME)) {
             const saida = outputarr[1].split(' ');
+            const outputstr = saida[saida.length - 1].replace('\n', '');
+
             if (saida.length === 3) {
-                res.status(200).json({ "dias": "0", "duracao": saida[saida.length - 1] });
+                res.status(200).json({ "dias": "0", "duracao": outputstr });
             } else if (saida.length >= 4) {
-                res.status(200).json({ "dias": saida[2], "duracao": saida[saida.length - 1] });
+                res.status(200).json({ "dias": saida[2], "duracao": outputstr });
             } else {
                 res.status(422).json({ errors: [{ msg: 'Não foi possível parsear resultado', param: null }] });
             }
