56abb4ecbecdc6ae5bac053f9a941f40636085a2
danizavtz <daniellucena@yahoo.com.br>
2024-07-25 14:10:33 -0300

    Fix values for atributes

--- server/routes/credentials.route.js
+++ server/routes/credentials.route.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const credentialsValidator = require('../validators/credentials.validator');
 const credentialsController = require('../controllers/credentials.controller');
 
-router.post('/validate', credentialsValidator.validationQueryRules, credentialsValidator.checkRules, credentialsController.verifyCredentialsInDjango, credentialsController.validado);
+router.post('/login', credentialsValidator.validationQueryRules, credentialsValidator.checkRules, credentialsController.verifyCredentialsInDjango, credentialsController.validado);
 
 module.exports = router;
